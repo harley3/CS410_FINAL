47c967fc541718f411b9041e8909ecf0102cf0b4
conradharley <harley3@illinois.edu>
2018-11-18 08:55:10 -0700

    Increased Java Heap Size and added 19997 USENet News Articles

--- CS410_FinalProject/queryForm.php
+++ CS410_FinalProject/queryForm.php
@@ -1,6 +1,6 @@
 <?php
 $querySTR = $_POST['query'];
-exec("java -jar QueryManager.jar " . $querySTR . " 2>&1", $output);
+exec("java -jar -Xmx512M QueryManager.jar " . $querySTR . " 2>&1", $output);
 ?>
 <CENTER><H1>Search Results for: <?php echo $querySTR; ?></H1></CENTER>
 
